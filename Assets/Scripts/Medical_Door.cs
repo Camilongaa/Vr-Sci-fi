807b8a796ac0c25b78d805233cb46863acbcb2f2
Camilongaa <jcgonzalez287@misena.edu.co>
2022-10-12 16:37:42 -0500

    12/10

--- Assets/Scripts/Medical_Door.cs
+++ Assets/Scripts/Medical_Door.cs
@@ -1,18 +1,35 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Medical_Door : MonoBehaviour
+
+    
 {
+    public GameObject Door;
     // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
+        if (other.tag == "Key_Card")
+
+
+        {
+            Door.GetComponent 
+
+        }
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+       
+
+
     }
 }
+
+*/
