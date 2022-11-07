c52ff14229bd7d795f1f72a01f388f6e730af555
Camilongaa <jcgonzalez287@misena.edu.co>
2022-11-07 09:25:09 -0500

    06/11/2022

--- Assets/Scripts/Puzzle_SalaMedica.cs
+++ Assets/Scripts/Puzzle_SalaMedica.cs
@@ -2,17 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Puzzle_SalaMedica : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [Header("Audios")]
+    public AudioSource CorrectAnswer;
+    public AudioSource WrongAnswer;
+
+    [Header("Niveles")]
+
+    public GameObject Level_one;
+    public GameObject Level_Two;
+    public GameObject Level_Three;
+
+    [Header("Tarjeta")]
+    public GameObject Tarjeta;
+
+    public void Start()
     {
+        Level_one.SetActive(true);
+        Level_Two.SetActive(false);
+        Level_Three.SetActive(false);
+
+        Tarjeta.SetActive(false);
+
+
+    }
+
+    public void RightAnswer1()
+    {
+        CorrectAnswer.Play();
+        Level_one.SetActive(false);
+        Level_Two.SetActive(true);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RightAnswer2()
     {
-        
+        CorrectAnswer.Play();
+        Level_Two.SetActive(false);
+        Level_Three.SetActive(true);
+
     }
+
+    public void RightAnswer3()
+    {
+        CorrectAnswer.Play();
+        Level_Two.SetActive(false);
+        Level_Three.SetActive(true);
+
+        Tarjeta.SetActive(true);
+    }
+
+    public void Wronganswer()
+    {
+        WrongAnswer.Play();
+    }
+
 }
