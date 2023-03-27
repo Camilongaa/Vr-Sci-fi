d2e230df997fcbe1b7c05d7f805a668b464f3d63
Camilongaa <jcgonzalez287@misena.edu.co>
2023-03-27 13:54:09 -0500

    27/03/2023

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
